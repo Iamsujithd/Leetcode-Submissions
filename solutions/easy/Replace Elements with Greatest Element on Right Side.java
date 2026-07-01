1512841e3b1a371dc040fde4919a47f7448e4922
Iamsujithd <146457405+Iamsujithd@users.noreply.github.com>
2026-07-01 22:43:53 +0530

    Add solution for Replace Elements with Greatest Element on Right Side

---
+++ solutions/easy/Replace Elements with Greatest Element on Right Side.java
@@ -0,0 +1,19 @@
+// Title: Replace Elements with Greatest Element on Right Side
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
+
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int max=-1,ele;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                ele=arr[j];
+            }
+        }
+    }
+                max=Math.max(max,ele);
+            arr[i]=max;
+        return arr;
+            max=-1;
+}
