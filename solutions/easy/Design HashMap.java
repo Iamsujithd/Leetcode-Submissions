7a9301ce38668280744ac07dd4291da498adc7bd
Iamsujithd <146457405+Iamsujithd@users.noreply.github.com>
2026-07-02 23:49:56 +0530

    Add solution for Design HashMap

---
+++ solutions/easy/Design HashMap.java
@@ -0,0 +1,31 @@
+// Title: Design HashMap
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/design-hashmap/
+
+            }
+                return l.get(1);
+            if(l.get(0)==key){
+        for(List<Integer> l:map){
+    public int get(int key) {
+    
+    }
+        map.add(i);
+        i.add(key);i.add(value);
+        List<Integer> i=new ArrayList<>();
+        }
+        return -1;
+    }
+    
+    public void remove(int key) {
+        for(List<Integer> l:map){
+            if(l.get(0)==key){
+                map.remove(l);
+            }
+        }
+    }
+}
+
+/**
+ * Your MyHashMap object will be 
+                return;
