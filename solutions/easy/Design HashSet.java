18ca81ebc29cc68e6766bd8b50acc7bb1f3c46e8
Iamsujithd <146457405+Iamsujithd@users.noreply.github.com>
2026-07-02 23:20:14 +0530

    Add solution for Design HashSet

---
+++ solutions/easy/Design HashSet.java
@@ -0,0 +1,30 @@
+// Title: Design HashSet
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/design-hashset/
+
+    public boolean contains(int key) {
+        return set[key];
+    }
+}
+
+/**
+        set[key]=true;
+    }
+    
+    public void remove(int key) {
+        set[key]=false;
+    }
+    
+    public void add(int key) {
+ * Your MyHashSet object will be 
+ instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+    
+    }
+        
+    public MyHashSet() {
+    
+    public boolean[] set=new boolean[p];
+    public static final int p=1000000;    
+class MyHashSet {
